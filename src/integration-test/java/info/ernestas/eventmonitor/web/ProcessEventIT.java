d2598b41a13f755189e8d9188aca9848effbdffe
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-08-29 10:49:54 +0300

    Instead of loading a full application context to embedded Tomcat - just load the WebSocket configuration

--- src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
+++ src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import info.ernestas.eventmonitor.AbstractTest;
 import info.ernestas.eventmonitor.configs.ApplicationTestConfiguration;
 import info.ernestas.eventmonitor.configs.IntegrationTestConfiguration;
+import info.ernestas.eventmonitor.configs.WebSocketConfiguration;
 import info.ernestas.eventmonitor.embedded.EmbeddedServer;
 import info.ernestas.eventmonitor.embedded.TomcatEmbeddedServer;
 import info.ernestas.eventmonitor.websocket.IncomingWebSocketRequestsHandler;
@@ -22,6 +23,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import java.util.concurrent.CountDownLatch;
@@ -59,8 +61,11 @@ public class ProcessEventIT extends AbstractTest {
 
     @Before
     public void setUp() throws Exception {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(WebSocketConfiguration.class);
+
         server = new TomcatEmbeddedServer(TOMCAT_PORT, testFolder.getRoot().getAbsolutePath());
-        server.deployConfig(webApplicationContext);
+        server.deployConfig(applicationContext);
         server.start();
 
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
