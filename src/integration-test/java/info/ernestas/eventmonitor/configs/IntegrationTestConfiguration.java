b73a56ee404eec7966a555517f01608c70bde7d5
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-08-29 09:27:57 +0300

    Upgraded Gradle to 3.0 and added some cleanups for integration tests

---
+++ src/integration-test/java/info/ernestas/eventmonitor/configs/IntegrationTestConfiguration.java
@@ -0,0 +1,47 @@
+package info.ernestas.eventmonitor.configs;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.JettyXhrTransport;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class IntegrationTestConfiguration {
+
+    private static final int MAX_THREADS = 10;
+
+    @Bean
+    public SockJsClient sockJsClient() throws Exception {
+        HttpClient jettyHttpClient = new HttpClient();
+        jettyHttpClient.setMaxConnectionsPerDestination(MAX_THREADS);
+        jettyHttpClient.setExecutor(new QueuedThreadPool(MAX_THREADS));
+        jettyHttpClient.start();
+
+        List<Transport> transports = new ArrayList<>();
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+        transports.add(new JettyXhrTransport(jettyHttpClient));
+
+        return new SockJsClient(transports);
+    }
+
+    @Bean
+    public WebSocketStompClient webSocketStompClient(SockJsClient sockJsClient) {
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        stompClient.setTaskScheduler(new DefaultManagedTaskScheduler());
+
+        return stompClient;
+    }
+
+}
--- src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
+++ src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
@@ -3,11 +3,10 @@ package info.ernestas.eventmonitor.web;
 import com.google.gson.Gson;
 import info.ernestas.eventmonitor.AbstractTest;
 import info.ernestas.eventmonitor.configs.ApplicationTestConfiguration;
+import info.ernestas.eventmonitor.configs.IntegrationTestConfiguration;
 import info.ernestas.eventmonitor.embedded.EmbeddedServer;
 import info.ernestas.eventmonitor.embedded.TomcatEmbeddedServer;
 import info.ernestas.eventmonitor.websocket.IncomingWebSocketRequestsHandler;
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,8 +15,6 @@ import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -25,22 +22,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.JettyXhrTransport;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {ApplicationTestConfiguration.class})
+@ContextConfiguration(classes = {ApplicationTestConfiguration.class, IntegrationTestConfiguration.class})
 @WebAppConfiguration
 public class ProcessEventIT extends AbstractTest {
 
@@ -64,6 +54,9 @@ public class ProcessEventIT extends AbstractTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
 
+    @Autowired
+    private WebSocketStompClient webSocketStompClient;
+
     @Before
     public void setUp() throws Exception {
         server = new TomcatEmbeddedServer(TOMCAT_PORT, testFolder.getRoot().getAbsolutePath());
@@ -82,10 +75,7 @@ public class ProcessEventIT extends AbstractTest {
     public void testProcessEvent() throws Exception {
         IncomingWebSocketRequestsHandler producer = new IncomingWebSocketRequestsHandler(errorCounter);
 
-        WebSocketStompClient stompClient = new WebSocketStompClient(getSockJsClient());
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-        stompClient.setTaskScheduler(new DefaultManagedTaskScheduler());
-        stompClient.connect("ws://localhost:" + TOMCAT_PORT + "/events", producer);
+        webSocketStompClient.connect("ws://localhost:" + TOMCAT_PORT + "/events", producer);
 
         Gson gson = new Gson();
 
@@ -94,20 +84,7 @@ public class ProcessEventIT extends AbstractTest {
         // Wait no more than two seconds
         errorCounter.await(TWO_SECONDS, TimeUnit.MILLISECONDS);
 
-        assertEquals(1, errorCounter.getCount());
-    }
-
-    private SockJsClient getSockJsClient() throws Exception {
-        HttpClient jettyHttpClient = new HttpClient();
-        jettyHttpClient.setMaxConnectionsPerDestination(10);
-        jettyHttpClient.setExecutor(new QueuedThreadPool(10));
-        jettyHttpClient.start();
-
-        List<Transport> transports = new ArrayList<>();
-        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
-        transports.add(new JettyXhrTransport(jettyHttpClient));
-
-        return new SockJsClient(transports);
+        assertEquals(NO_ERROR, errorCounter.getCount());
     }
 
 }
