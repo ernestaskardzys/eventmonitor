b9654a334a21316211197d54415772cc0717cb38
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2016-08-29 13:26:44 +0300

    Refactoring of embedded tomcat
    * Moved Tomcat to a separate bean
    
    * Moved Tomcat port to properties file

--- src/integration-test/java/info/ernestas/eventmonitor/configs/IntegrationTestConfiguration.java
+++ src/integration-test/java/info/ernestas/eventmonitor/configs/IntegrationTestConfiguration.java
@@ -1,11 +1,15 @@
 package info.ernestas.eventmonitor.configs;
 
+import info.ernestas.eventmonitor.embedded.EmbeddedServer;
+import info.ernestas.eventmonitor.embedded.TomcatEmbeddedServer;
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.JettyXhrTransport;
@@ -20,6 +24,9 @@ import java.util.List;
 public class IntegrationTestConfiguration {
 
     private static final int MAX_THREADS = 10;
+
+    @Value("${tomcat.port}")
+    private int tomcatPort;
 
     @Bean
     public SockJsClient sockJsClient() throws Exception {
@@ -42,6 +49,21 @@ public class IntegrationTestConfiguration {
         stompClient.setTaskScheduler(new DefaultManagedTaskScheduler());
 
         return stompClient;
+    }
+
+    @Bean
+    public EmbeddedServer embeddedServer() throws Exception {
+        String tempDirectory = System.getProperty("java.io.tmpdir");
+
+        String tomcatBaseDirectory = tempDirectory + "/tomcat." + tomcatPort;
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(WebSocketConfiguration.class);
+
+        EmbeddedServer server = new TomcatEmbeddedServer(tomcatPort, tomcatBaseDirectory);
+        server.deployConfig(applicationContext);
+        server.start();
+
+        return server;
     }
 
 }
--- src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
+++ src/integration-test/java/info/ernestas/eventmonitor/web/ProcessEventIT.java
@@ -4,17 +4,14 @@ import com.google.gson.Gson;
 import info.ernestas.eventmonitor.AbstractTest;
 import info.ernestas.eventmonitor.configs.ApplicationTestConfiguration;
 import info.ernestas.eventmonitor.configs.IntegrationTestConfiguration;
-import info.ernestas.eventmonitor.configs.WebSocketConfiguration;
 import info.ernestas.eventmonitor.embedded.EmbeddedServer;
-import info.ernestas.eventmonitor.embedded.TomcatEmbeddedServer;
 import info.ernestas.eventmonitor.websocket.IncomingWebSocketRequestsHandler;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -23,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 import java.util.concurrent.CountDownLatch;
@@ -36,22 +32,21 @@ import static org.junit.Assert.assertEquals;
 @WebAppConfiguration
 public class ProcessEventIT extends AbstractTest {
 
-    private static final int TOMCAT_PORT = 10000;
-
     private static final int TWO_SECONDS = 2000;
 
     private static final int TWO_MINUTES = 120000;
 
     private static final int NO_ERROR = 1;
 
-    @Rule
-    public TemporaryFolder testFolder = new TemporaryFolder();
-
     private MockMvc mockMvc;
 
-    private EmbeddedServer server;
-
     private CountDownLatch errorCounter = new CountDownLatch(NO_ERROR);
+
+    @Value("${tomcat.port}")
+    private int tomcatPort;
+
+    @Autowired
+    private EmbeddedServer server;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -61,13 +56,6 @@ public class ProcessEventIT extends AbstractTest {
 
     @Before
     public void setUp() throws Exception {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
-        applicationContext.register(WebSocketConfiguration.class);
-
-        server = new TomcatEmbeddedServer(TOMCAT_PORT, testFolder.getRoot().getAbsolutePath());
-        server.deployConfig(applicationContext);
-        server.start();
-
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
@@ -80,7 +68,7 @@ public class ProcessEventIT extends AbstractTest {
     public void testProcessEvent() throws Exception {
         IncomingWebSocketRequestsHandler producer = new IncomingWebSocketRequestsHandler(errorCounter);
 
-        webSocketStompClient.connect("ws://localhost:" + TOMCAT_PORT + "/events", producer);
+        webSocketStompClient.connect("ws://localhost:" + tomcatPort + "/events", producer);
 
         Gson gson = new Gson();
 
